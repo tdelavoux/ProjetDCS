e67913f1c394c91f7dd735222ebc4833f50c003e
RBleu <raphaelbleu70@gmail.com>
2018-12-03 11:21:09 +0100

    Address/P2PFile

--- ProjetDCS/src/CommServCli/Address.java
+++ ProjetDCS/src/CommServCli/Address.java
@@ -1,5 +1,64 @@
-package CommServCli;
+package servcli;
 
-public class Address {
+public class Address implements Comparable<Address>{
+	private String addressIp;
+	private int port;
+	
+	public Address(String addressIp, int port)
+	{
+		this.addressIp = addressIp;
+		this.port = port;
+	}
+	
+	public String getAddressIp() { return addressIp; }
+	public int getPort() { return port; }
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((addressIp == null) ? 0 : addressIp.hashCode());
+		result = prime * result + port;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (addressIp == null) {
+			if (other.addressIp != null)
+				return false;
+		} else if (!addressIp.equals(other.addressIp))
+			return false;
+		if (port != other.port)
+			return false;
+		return true;
+	}
+
+	public int compareTo(Address a) {
+		if(a.addressIp.equals(this.addressIp))
+		{
+			if(a.port < this.port)
+			{
+				return 1;
+			}
+			if(a.port > this.port)
+			{
+				return -1;
+			}
+		}
+		else
+		{
+			return 1;
+		}	
+		return 0;
+	}
+	
+	
 }
--- ProjetDCS/src/CommServCli/P2PFile.java
+++ ProjetDCS/src/CommServCli/P2PFile.java
@@ -1,8 +1,45 @@
-package CommServCli;
+package servcli;
 
 public class P2PFile {
+	private long taille;
+	private String name;
 	
-	public P2PFile() {
-		
+	public P2PFile(long taille, String name)
+	{
+		this.taille = taille;
+		this.name = name;
+	}	
+	
+	public long getTaille() { return taille; }
+	public String getName() { return name; }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + (int) (taille ^ (taille >>> 32));
+		return result;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		P2PFile other = (P2PFile) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (taille != other.taille)
+			return false;
+		return true;
+	}
+	
+	
 }
